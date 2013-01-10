5b4affefcf91fe78ce98530bf5108b950e19ae1f
Jeremy Bowman <jmbowman@safaribooksonline.com>
2013-01-10 16:37:34 -0500

    Packaging fixes

--- setup.py
+++ setup.py
@@ -8,8 +8,17 @@ setup(
     author="Jeremy Bowman",
     author_email="jbowman@safaribooksonline.com",
     description="Integrate QUnit JavaScript tests into a Django test suite via nose",
-    include_package_data=True,
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    package_data={
+        'django_nose_qunit': [
+            'run-qunit.js',
+            'static/*.css',
+            'static/*.js'
+            'static/django_nose_qunit/test/*.js',
+            'templates/django_nose_qunit/*.html',
+            'templates/django_nose_qunit/fixtures/*.html',
+        ],
+    },
     zip_safe=False,
     install_requires=[
         'Django>=1.4',
@@ -21,4 +30,13 @@ setup(
             'django-qunit = django_nose_qunit.nose_plugin:QUnitPlugin'
         ]
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Software Development :: Testing',
+    ],
 )
