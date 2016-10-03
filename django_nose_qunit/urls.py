0812d55c3ddead3289796906c1ab7f1f5bba159f
Jorge Rodríguez-Flores Esparza <jorrete@gmail.com>
2016-10-03 10:04:10 +0200

    urls import failing in django 1.10

--- django_nose_qunit/urls.py
+++ django_nose_qunit/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
+from .views import test_index, run_qunit_tests
 
 # This should be safe to leave in the URL configuration even in production;
 # the view always returns a 404 if the test classes haven't been loaded
 
-urlpatterns = patterns(
-    'django_nose_qunit.views',
-    (r'qunit/$', 'test_index', {}, 'django-nose-qunit-list'),
-    (r'^qunit/test/$', 'run_qunit_tests', {}, 'django-nose-qunit-test')
-)
+
+urlpatterns = [
+    url(r'qunit/$', test_index, name='django-nose-qunit-list'),
+    url(r'^qunit/test/$', run_qunit_tests,name='django-nose-qunit-test')
+]
